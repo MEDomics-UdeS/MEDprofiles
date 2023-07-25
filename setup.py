c737b438b345ddceaa7e4f86ff200163b074ce81
Sari27 <sarah.dns.sd@gmail.com>
2023-07-25 09:58:05 -0400

    Test package

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ setup(name='MEDprofiles',
       author='Sarah Denis',
       author_email='dens1704@usherbrooke.ca',
       url='https://github.com/MEDomics-UdeS/MEDprofiles',
-      packages=find_packages(exclude=['docs']),
+      package_dir={"": "src"},
+      packages=find_packages(),
       install_requires=requirements
       )
