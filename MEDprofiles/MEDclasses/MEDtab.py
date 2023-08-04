d226bfe12de133019f01184d68d232dcf986e8c9
Sari27 <sarah.dns.sd@gmail.com>
2023-08-04 15:13:36 -0400

    Update MEDtab.py

--- MEDprofiles/MEDclasses/MEDtab.py
+++ MEDprofiles/MEDclasses/MEDtab.py
@@ -1,6 +1,6 @@
 import datetime
 from pydantic import validator
-from ..MEDclasses import *
+from MEDprofiles.MEDclasses import *
 from typing import Optional
 from MEDprofiles.src.back.constant import DATE_FORMAT
 
